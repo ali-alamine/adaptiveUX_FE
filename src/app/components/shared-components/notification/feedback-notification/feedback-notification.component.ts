59dc27cc07e5fade2bf3eb2589aa95b3421ccc9b
Ali Al Amine <ali.alamine1992@gmail.com>
2023-11-22 19:31:48 +0000

    notification part 1

---
+++ src/app/components/shared-components/notification/feedback-notification/feedback-notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pu-feedback-notification',
+  templateUrl: './feedback-notification.component.html',
+  styleUrls: ['./feedback-notification.component.scss']
+})
+export class FeedbackNotificationComponent {
+
+}
